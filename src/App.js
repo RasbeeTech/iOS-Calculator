ef06e6efafec292da0daa0dbbf7837cd122feb7e
MykalB <mykalb081@gmail.com>
2021-06-05 19:29:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,24 +31,22 @@ class App extends React.Component {
       );});
     
     return (
-      <div className='container-fluid'>
-        <div className='bg-success'>
-          <div className='h-100 row w-75'>
-            <div id='display' className='col-12 bg-warning'>{this.state.output}</div>
-            <button id='clear' className ='col-3' onClick={this.clear}>C</button>
-            <button className='col-3'>+/-</button>
-            <button id='percent' className = 'col-3'>%</button>
-            {arithematics[3]}
-            {numbers.slice(7,10)}
-            {arithematics[2]}
-            {numbers.slice(4,7)}
-            {arithematics[1]}
-            {numbers.slice(1,4)}
-            {arithematics[0]}
-            {numbers.slice(0,1)}
-            <button id='decimal' className='col-3 bg-secondary text-white'>.</button>
-            <button id='equals' className='col-3 bg-warning text-white'>{'='}</button>
-          </div>
+      <div className='container w-50'>
+        <div className='row' style={{height: '75vh', marginTop:60}}>
+          <div id='display' className='col-12 bg-warning'>{this.state.output}</div>
+          <button id='clear' className ='col-3' onClick={this.clear}>C</button>
+          <button className='col-3'>+/-</button>
+          <button id='percent' className = 'col-3'>%</button>
+          {arithematics[3]}
+          {numbers.slice(7,10)}
+          {arithematics[2]}
+          {numbers.slice(4,7)}
+          {arithematics[1]}
+          {numbers.slice(1,4)}
+          {arithematics[0]}
+          {numbers.slice(0,1)}
+          <button id='decimal' className='col-3 bg-secondary text-white'>.</button>
+          <button id='equals' className='col-3 bg-warning text-white'>{'='}</button>
         </div>
       </div>
     );
