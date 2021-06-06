129b7949c921baeac99e38141c0f7db0f0728de6
MykalB <mykalb081@gmail.com>
2021-06-05 22:44:26 -0400

    update

--- src/App.js
+++ src/App.js
@@ -9,27 +9,47 @@ class App extends React.Component {
       ariths: ['add','subtract','multiply','divide'],
       symbols: ['+','-','X','/'],
       input: 0,
-      output: 0
+      output: '0'
     };
     this.clear = this.clear.bind(this);
+    this.numberPress = this.numberPress.bind(this);
+    this.operation = this.operation.bind(this);
   }
   clear(){
     this.setState({
       input: 0,
-      output: 0
+      output: '0'
     });
+  }
+  
+  numberPress(value){
+    this.setState((state) => {
+      return {output: state.output !== '0' ? state.output + value.toString() : value.toString()}
+    });
+  }
+  operation(value){
+    console.log(value);
+    switch(value){
+      case 'add':
+      case 'subract':
+      case 'multiply':
+      case 'divide':
+      case 'equals':
+      case 'decimal':
+      case 'percent':
+      case 'negative':
+    }
   }
 
   render(){
     let numbers = this.state.nums.map((x, index) => {
       return(
-          <button id={x} 
-          className={x !== 'zero' ? 'col-3 rounded-circle' : 'col-6 rounded-pill'}>{index}</button>
+          <button id={x} onClick={() => this.numberPress(index)}className={x !== 'zero' ? 'col-3 rounded-circle' : 'col-6 rounded-pill'}>{index}</button>
       );});
 
     let arithematics = this.state.ariths.map((x, index) => {
       return(
-        <button id={x} className='col-3 rounded-circle'>{this.state.symbols[index]}</button>
+        <button id={x} onClick={() => this.operation(x)}className='col-3 rounded-circle'>{this.state.symbols[index]}</button>
       );});
     
     return (
@@ -37,8 +57,8 @@ class App extends React.Component {
         <div className='row' style={{height: '90vh', marginTop:20}}>
           <div id='display' className='col-12'><span>{this.state.output}</span></div>
           <button id='clear' className ='col-3 rounded-circle' onClick={this.clear}>C</button>
-          <button id='negative'className='col-3 rounded-circle'>+/-</button>
-          <button id='percent' className = 'col-3 rounded-circle'>%</button>
+          <button id='negative' onClick={() => this.operation('negative')} className='col-3 rounded-circle'>+/-</button>
+          <button id='percent' onClick={() => this.operation('percent')} className = 'col-3 rounded-circle'>%</button>
           {arithematics[3]}
           {numbers.slice(7, 10)}
           {arithematics[2]}
@@ -47,8 +67,8 @@ class App extends React.Component {
           {numbers.slice(1, 4)}
           {arithematics[0]}
           {numbers.slice(0, 1)}
-          <button id='decimal' className='col-3 rounded-circle'>.</button>
-          <button id='equals' className='col-3 rounded-circle'>=</button>
+          <button id='decimal' onClick={() => this.operation('decimal')} className='col-3 rounded-circle'>.</button>
+          <button id='equals' onClick={() => this.operation('equals')} className='col-3 rounded-circle'>=</button>
         </div>
       </div>
     );
