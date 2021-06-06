cdd57d5d7d379d3a9cfc521b877e9e1eb6d12e6e
MykalB <mykalb081@gmail.com>
2021-06-06 15:21:17 -0400

    update
    - change background color
    - completed functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css'
 
+const NUMS = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+const ARITHS = ['add','subtract','multiply','divide'];
+const SYMS = ['+','-','X','/'];
+
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      nums: ['zero','one','two','three','four','five','six','seven','eight','nine'],
-      ariths: ['add','subtract','multiply','divide'],
-      symbols: ['+','-','X','/'],
-      input: 0,
+      input: [],
+      newInput: false,
       output: '0'
     };
     this.clear = this.clear.bind(this);
@@ -17,39 +19,84 @@ class App extends React.Component {
   }
   clear(){
     this.setState({
-      input: 0,
+      prevInput: 0,
+      input: [],
+      newInput: false,
       output: '0'
     });
   }
-  
   numberPress(value){
-    this.setState((state) => {
-      return {output: state.output !== '0' ? state.output + value.toString() : value.toString()}
-    });
+    let output = '';
+    if(this.state.output === '0'){
+      output = value.toString();
+    } else if(this.state.newInput) {
+      output = value.toString();
+      this.setState({newInput: false});
+    } else {
+      output = this.state.output + value.toString();
+    }
+    this.setState({output: output});
   }
   operation(value){
-    console.log(value);
     switch(value){
       case 'add':
-      case 'subract':
+      case 'subtract':
       case 'multiply':
       case 'divide':
-      case 'equals':
+        if(this.state.input.length == 2 && !SYMS.includes(this.state.input[1])){
+          this.evaluate();
+        } else{
+          this.setState(() => {
+            return {input: [this.state.output, value], newInput: true};
+          }, ()=> console.log(this.state.input));
+        }
+        break;
       case 'decimal':
-      case 'percent':
+        this.setState({output: this.state.output.includes('.') ? this.state.output :this.state.output.concat('.')});
+        break;
       case 'negative':
+        this.setState({output: (Number(this.state.output) * -1).toString()});
+        break;
+      case 'percent':
+        this.setState({output: (Number(this.state.output) * 0.01).toString()});
+        break;
+      case 'equals':
+        this.evaluate();
+        break;
+    }
+  }
+  evaluate(){
+    let num1 = Number(this.state.input[0]);
+    let num2 = Number(this.state.output);
+    switch(this.state.input[1]){
+      case 'add':
+        this.setState({output: (num1 + num2).toString(), input: [(num1 + num2).toString(), 'add'], newInput: true},
+          () => console.log(this.state.output, this.state.input));
+        break;
+      case 'subtract':
+        this.setState({output: (num1 - num2).toString(), input: [(num1 - num2).toString(), 'subtract'], newInput: true},
+          () => console.log(this.state.output, this.state.input));
+        break;
+      case 'multiply':
+        this.setState({output: (num1 * num2).toString(), input: [(num1 * num2).toString(), 'multiply'], newInput: true},
+          () => console.log(this.state.output, this.state.input));
+        break;
+      case 'divide':
+        this.setState({output: (num1 / num2).toString(), input: [(num1 / num2).toString(), 'divide'], newInput: true},
+          () => console.log(this.state.output, this.state.input));
+        break;
     }
   }
 
   render(){
-    let numbers = this.state.nums.map((x, index) => {
+    let numbers = NUMS.map((x, index) => {
       return(
-          <button id={x} onClick={() => this.numberPress(index)}className={x !== 'zero' ? 'col-3 rounded-circle' : 'col-6 rounded-pill'}>{index}</button>
+        <button id={x} onClick={() => this.numberPress(index)} className={x !== 'zero' ? 'col-3 rounded-circle' : 'col-6 rounded-pill'}>{index}</button>
       );});
 
-    let arithematics = this.state.ariths.map((x, index) => {
+    let arithematics = ARITHS.map((x, index) => {
       return(
-        <button id={x} onClick={() => this.operation(x)}className='col-3 rounded-circle'>{this.state.symbols[index]}</button>
+        <button id={x} onClick={() => this.operation(x)}className='col-3 rounded-circle'>{SYMS[index]}</button>
       );});
     
     return (
